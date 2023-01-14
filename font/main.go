5c0d9c3cd289aa4eda0ad04bbe467257fcc32347
kajikentaro <kajindowsxp@gmail.com>
2023-01-14 19:46:26 +0900

    Fix: 呼び出す関数を変更

--- font/main.go
+++ font/main.go
@@ -15,7 +15,9 @@ func main() {
 func drawTtf() {
 	dc := gg.NewContext(1200, 628)
 
-	face := fetchPostScriptFontFace("resource/GoNotoCJKCore.ttf")
+	face := fetchTrueTypeFontFace("resource/GoNotoCJKCore.ttf")
+	// PostScript は TrueTypeに対する後方互換性がある(?). ↓でも動く.
+	// face := fetchPostScriptFontFace("resource/GoNotoCJKCore.ttf")
 	dc.SetFontFace(face)
 	dc.SetColor(color.White)
 	dc.DrawStringWrapped("あいうえお", 50, 200, 0, 0, 600, 1, gg.AlignRight)
